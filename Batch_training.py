31ee62128dda023f55aec969875bf271e8979656
Ruey-Day <rueyday1@gmail.com>
2025-03-06 23:45:44 -0500

    finished batch training

---
+++ Batch_training.py
@@ -0,0 +1,43 @@
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import math
+
+class WineDataset(Dataset):
+    def __init__(self):
+        # data loading
+        xy = np.loadtxt('wine.csv', delimiter=',', dtype=np.float32, skiprows=1)
+        self.n_samples = xy.shape[0]
+
+        # note that we do not convert to tensor here
+        self.x_data = torch.from_numpy(xy[:, 1:])
+        self.y_data = torch.from_numpy(xy[:, [0]]) # n_samples, 1
+
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+    def __len__(self):
+        return self.n_samples
+    
+dataset = WineDataset()
+# first_data = dataset[0]
+# features, labels = first_data
+# print(features, labels)
+
+dataloader = DataLoader(dataset=dataset, batch_size=4, shuffle=True, num_workers=0)
+datatiter = iter(dataloader)
+data = next(datatiter)
+features, labels = data
+print(features, labels)
+
+# training loop
+num_epochs = 2
+total_samples = len(dataset)
+n_iterations = math.ceil(total_samples/4)
+print(total_samples, n_iterations)
+
+for epoch in range(num_epochs):
+    for i, (inputs, labels) in enumerate(dataloader):
+        # forward, backward, update
+        if (i+1) % 5 == 0:
+            print(f'epoch {epoch+1}/{num_epochs}, step {i+1}/{n_iterations}, inputs {inputs.shape}')
